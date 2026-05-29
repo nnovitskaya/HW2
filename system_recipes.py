87ea9388dd9b58e950a3724fb63eeaf619fd16a7
Anastasia Chepasova <aachepasova@edu.hse.ru>
2026-05-30 00:24:31 +0300

    Реализован класс Recipe

--- system_recipes.py
+++ system_recipes.py
@@ -1,3 +1,5 @@
+import copy
+
 class Ingredient:
     def __init__(self, name, quantity, unit):
         self.name = name
@@ -24,3 +26,37 @@ class Ingredient:
         return self.name == other.name and self.unit == other.unit
 
 
+class Recipe:
+    def __init__(self, title, ingredients):
+        self.title = title
+        self.ingredients = ingredients
+
+    def add_ingredient(self, ingredient: Ingredient):
+
+        for current_ingredient in self.ingredients:
+            if current_ingredient.name == ingredient.name and current_ingredient.unit == ingredient.unit:
+                current_ingredient.quantity += ingredient.quantity
+                return
+
+        self.ingredients.append(ingredient)
+    
+    @staticmethod
+    def is_valid_ratio(ratio):
+        return isinstance(ratio, (int, float)) and ratio > 0
+
+    def scale(self, ratio: float):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError("Коэффициент должен являться числом и быть положительным")
+
+        new_recipe = copy.deepcopy(self)
+
+        for ingredient in new_recipe.ingredients:
+            ingredient.quantity *= ratio
+
+        return new_recipe
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        return f"{self.title}: {', '.join(str(ingredient) for ingredient in self.ingredients)}"
