be8f24bb9ad229fd44768f0baace03fed9b24f48
Anastasia Chepasova <aachepasova@edu.hse.ru>
2026-05-30 13:21:42 +0300

    Реализованы тесты для класса Recipe

--- test_recipes.py
+++ test_recipes.py
@@ -13,6 +13,7 @@ def test_ingredient_str():
 
 def test_ingredient_eq_correctly():
     ingredient1 = Ingredient("Мука", 500.0, "г")
+
     ingredient2 = Ingredient("Мука", 400.0, "г")
     assert ingredient1 == ingredient2
 
@@ -26,3 +27,41 @@ def test_ingredient_eq_different_units():
     ingredient2 = Ingredient("Мука", 500.0, "кг")
     assert ingredient1 != ingredient2
 
+def test_recipe_init():
+    ingredients = [Ingredient("Мука", 500.0, "г"), Ingredient("Картошка", 300.0, "г"), Ingredient("Яйца", 2, "шт")]
+    recipe = Recipe("Пирожки", ingredients)
+    assert recipe.title == "Пирожки"
+    assert len(recipe.ingredients) == 3
+
+def test_recipe_add_ingredients():
+    ingredients = [Ingredient("Мука", 500.0, "г"), Ingredient("Картошка", 300.0, "г"), Ingredient("Яйца", 2, "шт")]
+    recipe = Recipe("Пирожки", ingredients)
+    recipe.add_ingredient(Ingredient("Грибы", 150.0, "г"))
+    recipe.add_ingredient(Ingredient("Мука", 100.0, "г"))
+    assert len(recipe.ingredients) == 4
+    assert recipe.ingredients[0].quantity == 600.0
+
+def test_recipe_scale_correctly():
+    ingredients = [Ingredient("Мука", 500.0, "г"), Ingredient("Картошка", 300.0, "г"), Ingredient("Яйца", 2, "шт")]
+    recipe = Recipe("Пирожки", ingredients)
+    scaled_recipe = recipe.scale(5)
+    assert scaled_recipe.title == "Пирожки"
+    assert len(scaled_recipe.ingredients) == 3
+    assert scaled_recipe.ingredients[0].quantity == 2500.0
+    assert scaled_recipe.ingredients[1].quantity == 1500.0
+    assert scaled_recipe.ingredients[2].quantity == 10
+
+def test_recipe_scale_error():
+    ingredients = [Ingredient("Мука", 500.0, "г"), Ingredient("Картошка", 300.0, "г"), Ingredient("Яйца", 2, "шт")]
+    recipe = Recipe("Пирожки", ingredients)
+    with pytest.raises(ValueError):
+        recipe.scale(-5)
+    with pytest.raises(ValueError):
+        recipe.scale(0)
+    with pytest.raises(ValueError):
+        recipe.scale("str")
+
+def test_recipe_len():
+    ingredients = [Ingredient("Мука", 500.0, "г"), Ingredient("Картошка", 300.0, "г"), Ingredient("Яйца", 2, "шт")]
+    recipe = Recipe("Пирожки", ingredients)
+    assert len(recipe) == 3
