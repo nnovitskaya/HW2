b55e030796cfd34e603fe3d12db241bd069eb6de
Anastasia Chepasova <aachepasova@edu.hse.ru>
2026-05-30 11:28:39 +0300

    Реализован класс DietaryRecipe

--- system_recipes.py
+++ system_recipes.py
@@ -107,3 +107,17 @@ class ShoppingList:
 
     def __add__(self, other: 'ShoppingList'):
         return ShoppingList(self._items + other._items)
+
+
+class DietaryRecipe(Recipe):
+    def __init__(self, title, diet_type, ingredients):
+        super().__init__(title, ingredients)
+        self.diet_type = diet_type
+    
+    def scale(self, ratio: float):
+        new_recipe = super().scale(ratio)
+
+        return DietaryRecipe(new_recipe.title, self.diet_type, new_recipe.ingredients)
+
+    def __str__(self):
+        return f"[{self.diet_type}]{self.title}: {', '.join(str(ingredient) for ingredient in self.ingredients)}"
